a8f5ed507ccd7d1a7da10ce7d31d75d584e311c7
iremseven <seveirem@gmail.com>
2026-04-18 23:01:13 +0300

    add rpg character creator script

---
+++ 08-rpg-character-creator.py
@@ -0,0 +1,34 @@
+# Project: RPG Character Creator
+# Source: freeCodeCamp Python Certification
+# Description: A script to create and validate RPG characters with visual stat bars using complex conditional logic.
+
+full_dot = '●'
+empty_dot = '○'
+
+def create_character(name, strength, intelligence, charisma):
+    if not isinstance(name, str):
+        return 'The character name should be a string'
+    if name == '':
+        return 'The character should have a name'
+    if len(name) > 10:
+        return 'The character name is too long'
+    if ' ' in name:
+        return 'The character name should not contain spaces'
+    
+    stats = [strength, intelligence, charisma]
+
+    if not all(isinstance(s, int) for s in stats):
+        return 'All stats should be integers'
+    if any(s < 1 for s in stats):
+        return 'All stats should be no less than 1'
+    if any(s > 4 for s in stats):
+        return 'All stats should be no more than 4'
+    if sum(stats) != 7:
+        return 'The character should start with 7 points'
+
+    str_bar = 'STR' + '●' * strength + '○' * (10 - strength)
+    int_bar = 'INT' + '●' * intelligence + '○' * (10 - intelligence)
+    cha_bar = 'CHA' + '●' * charisma + '○' * (10 - charisma)
+    return f'{name}\n{str_bar}\n{int_bar}\n{cha_bar}'
+
+print(create_character('ren', 4, 2, 1))
